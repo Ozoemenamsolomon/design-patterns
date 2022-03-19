8b49ff439a3fe18e68e1178781eef2b3ea125339
Solozo <ozoemenamsolomon@yahoo.com>
2022-03-19 21:19:07 +0100

    describe factory class

--- factory/CurrencyFormatterFactory.ts
+++ factory/CurrencyFormatterFactory.ts
@@ -1,6 +1,12 @@
 import { CurrencyFormat } from './concrete-formats';
 
+/**
+ * @description use the static create funtion to get a currency formatter object
+ */
 export default class CurrencyFormatterFactory {
+	/**
+	 * @description returns a currency formatter based on the currency format provided
+	 */
 	static create(currencyFormat: CurrencyFormat) {
 		return new Intl.NumberFormat(currencyFormat.locale, {
 			currency: currencyFormat.currency,
